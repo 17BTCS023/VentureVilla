af27b06f2eec6c987f2e91f5622d9fdfb43ba0b7
17BTCS023 <post.mk02@gmail.com>
2024-03-02 09:19:51 +0530

    fixed: post-login page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,15 @@
 module.exports.isLoggedin = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "Login to add a listing!");
         res.redirect("/login");
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
